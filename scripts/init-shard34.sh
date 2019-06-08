b767f98d62b7b42d9d11474333710725cac4fe8f
Matic Novak <maticn@analitica.pvt>
2019-06-08 20:13:19 +0200

    Updated scripts for the second computer.

--- scripts/init-shard34.sh
+++ scripts/init-shard34.sh
@@ -1,25 +1,15 @@
 #!/bin/bash 
 
-# mongodb1=`getent hosts ${MONGOS} | awk '{ print $1 }'`
-mongodb1="192.168.20.160"
+mongodbrouter="192.168.20.160"
+thisip="192.168.20.186"
 
-mongodb11=`getent hosts ${MONGO11} | awk '{ print $1 }'`
-mongodb12=`getent hosts ${MONGO12} | awk '{ print $1 }'`
-mongodb13=`getent hosts ${MONGO13} | awk '{ print $1 }'`
+port=${PORT:-27017}
 
-mongodb21=`getent hosts ${MONGO21} | awk '{ print $1 }'`
-mongodb22=`getent hosts ${MONGO22} | awk '{ print $1 }'`
-mongodb23=`getent hosts ${MONGO23} | awk '{ print $1 }'`
-
-mongodb31=`getent hosts ${MONGO31} | awk '{ print $1 }'`
-mongodb32=`getent hosts ${MONGO32} | awk '{ print $1 }'`
-mongodb33=`getent hosts ${MONGO33} | awk '{ print $1 }'`
-
-#port=${PORT:-27017}
-port="30001"
+echo $mongodbrouter
+echo $thisip
 
 echo "Waiting for startup.."
-until mongo --host ${mongodb1}:${port} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
+until mongo --host ${mongodbrouter}:${port} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
   printf '.'
   sleep 1
 done
@@ -27,8 +17,8 @@ done
 echo "Started.."
 
 echo init-shard34.sh time now: `date +"%T" `
-mongo --host ${mongodb1}:${port} <<EOF
-   sh.addShard( "${RS3}/${mongodb11}:${PORT1},${mongodb12}:${PORT2},${mongodb13}:${PORT3}" );
-   sh.addShard( "${RS4}/${mongodb21}:${PORT1},${mongodb22}:${PORT2},${mongodb23}:${PORT3}" );
+mongo --host ${mongodbrouter}:${port} <<EOF
+   sh.addShard( "${RS3}/${thisip}:${PORT1},${thisip}:${PORT2},${thisip}:${PORT3}" );
+   sh.addShard( "${RS4}/${thisip}:${PORT4},${thisip}:${PORT5},${thisip}:${PORT6}" );
    sh.status();
 EOF
